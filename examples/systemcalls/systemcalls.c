fe9ec1182ce5fa275e49eed8169edce9141dcdfa
mohamedkhaled127 <72905885+mohamedkhaled127@users.noreply.github.com>
2023-06-15 05:54:40 +0200

    assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,8 @@
 #include "systemcalls.h"
-
+#include <stdlib.h>
+#include <unistd.h>
+#include<sys/wait.h>
+#include <fcntl.h>
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -16,6 +19,11 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int temp=system(cmd);
+    if(temp !=0){
+
+        return false;
+    }
 
     return true;
 }
@@ -58,10 +66,28 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+   int status;
+    int pid=fork();
+    if(pid==0)     //child process
+    {
+        execv(command[0],command);
+        exit(1);
+       
+    }
 
+    if(pid>0)  //parent process
+    {
+       wait(&status);
+        if (WEXITSTATUS( status))   //exit 1
+	{
+	    return false;
+	}
+        return true;    //exit 0
+    }
+    
     va_end(args);
 
-    return true;
+    return false;
 }
 
 /**
@@ -92,8 +118,35 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+   int status;
+    int fd= open(outputfile, O_RDWR | O_CREAT);
+    if(fd<0)
+    {
+        return false;
+    }
 
+
+    int pid=fork();
+    if(pid==0)     //child process
+    {
+        if(dup2(fd,1)<0){
+            exit(1);
+        }
+        execv(command[0],command);
+        exit(1);
+    
+    }
+
+    if(pid>0)  //parent process
+    {
+        wait(&status);
+        if (WEXITSTATUS( status))   //exit 1
+	{
+	    return false;
+	}
+        return true;    //exit 0
+    }
     va_end(args);
-
-    return true;
+    close(fd);
+    return false;
 }
